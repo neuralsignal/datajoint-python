b3ecc6b5531e95481a58870a1131c1c89f46941f
gucky92 <matchristenson@gmail.com>
2018-08-31 14:07:57 -0400

    minor

--- datajoint/autopopulate.py
+++ datajoint/autopopulate.py
@@ -22,7 +22,6 @@ class AutoPopulate:
     must define the property `key_source`, and must define the callback method `make`.
     """
     _key_source = None
-    _multi_fetch = True
 
     @property
     def key_source(self):
@@ -32,7 +31,9 @@ class AutoPopulate:
                 The default value is the join of the parent relations.
                 Users may override to change the granularity or the scope of populate() calls.
         """
-        if hasattr(self, 'joined_table') and not self._multi_fetch: #TODO deal with multiple
+        if not hasattr(self, 'joined_table'):
+            self._multi_fetch = True
+        if not self._multi_fetch: #TODO deal with multiple
             self._key_source = self.joined_table
         if self._key_source is None:
             if self.target.full_table_name not in self.connection.dependencies:
@@ -84,7 +85,7 @@ class AutoPopulate:
                 'The populate target lacks attribute %s from the primary key of key_source' % next(
                     name for name in todo.heading.primary_key
                     if name not in self.target.heading
-                    and not (hasattr(self, 'joined_table') and not self._multi_fetch)
+                    and (hasattr(self, 'joined_table') and not self._multi_fetch)
                 ))
         except StopIteration:
             pass
